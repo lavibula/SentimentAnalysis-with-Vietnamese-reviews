4c5832c598c84d2bdecd8bfb9d0e40b2050b2fdc
Nguyễn Minh Tuấn <nmttuan1501@gmail.com>
2024-05-31 22:08:28 +0700

    Add custom transformer, initial trainer

---
+++ prepare_data.py
@@ -0,0 +1,47 @@
+from torch.utils.data import Dataset
+import torch
+import pandas as pd
+import logging
+
+TRAIN_DATA_PATH = "Data_preprocessed/Train.csv"
+VAL_DATA_PATH = "Data_preprocessed/Validation.csv"
+
+class SentimentDataset(Dataset):
+    def __init__(self, sentences, sentiment, topic, tokenizer = None, max_length = 256, padding = "longest", truncation=True, tokenized = False):
+        self.logger = logging.getLogger("SentimentDataset")
+        self.sentiment = sentiment
+        self.topic = topic
+        self.num_sentiments = len(set(sentiment))
+
+        # Tokenize the input sentences beforehand for faster loading
+        if tokenized:
+            self.tokenized = sentences
+        elif tokenizer is None:
+            raise ValueError(f"When {tokenized = }, tokenizer must not be None")
+        else:
+            self.logger.info("Tokenizing...")
+            self.tokenized = tokenizer(sentences, max_length = max_length, padding = padding, truncation = truncation)
+    
+    def __len__(self):
+        return len(self.topic)
+    
+    def __getitem__(self, idx):
+        token = {key: torch.tensor(val[idx]) for key, val in self.tokenized.items()}
+        topic = self.topic[idx]
+        sentiment = self.sentiment[idx]
+        gt = self.num_sentiments * topic + sentiment # Merge topic and sentiment into one label
+        token['label'] = torch.tensor(gt)
+        return token
+    
+def load_data(data_path, tokenizer, tokenized = False):
+    df = pd.read_csv(data_path).dropna()
+    sentences = df["sentence_dropped"].tolist()
+    sentiment = df["sentiment"].tolist()
+    topic = df["topic"].tolist()
+    return SentimentDataset(sentences, sentiment, topic, tokenizer, tokenized)
+
+if __name__ == "__main__":
+    from transformers import AutoTokenizer
+    tokenizer = AutoTokenizer.from_pretrained("vinai/phobert-base")
+    train_dataset = load_data(TRAIN_DATA_PATH, tokenizer)
+    print(train_dataset[0])
---
+++ train.py
@@ -0,0 +1,67 @@
+from custom_transformers import RobertaConfig, RobertaForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments
+import evaluate
+from prepare_data import load_data
+
+import logging
+
+import os
+os.environ["WANDB_PROJECT"]="VietnameseSentimentAnalysis"
+import wandb
+wandb.login(key="3f1b76982c335dfa2a93d09ae7cf4b34a640bc60")
+
+logging.basicConfig(level = logging.INFO)
+
+TRAIN_DATA_PATH = "Data_preprocessed/Train.csv"
+VAL_DATA_PATH = "Data_preprocessed/Validation.csv"
+PRETRAINED_MODEL = "vinai/phobert-base"
+NUM_LABELS = 16
+
+def compute_metrics(pred, metric):
+    logits, labels = pred
+    predictions = logits.argmax(-1)
+    return metric.compute(predictions = predictions, references = labels)
+
+def main():
+    tokenizer = AutoTokenizer.from_pretrained(PRETRAINED_MODEL)
+    config = RobertaConfig(problem_type = "single_label_classification", num_labels = NUM_LABELS)
+    classifier = RobertaForSequenceClassification(config).classifier
+    model = RobertaForSequenceClassification(config).from_pretrained("vinai/phobert-base")
+    model.classifier = classifier
+    model.num_labels = NUM_LABELS
+    train_dataset = load_data(TRAIN_DATA_PATH, tokenizer)
+    val_dataset = load_data(VAL_DATA_PATH, tokenizer)
+
+    metric = evaluate.load("accuracy")
+    compute_metric = lambda pred: compute_metrics(pred, metric)
+    
+    training_args = TrainingArguments(
+        output_dir = "output",
+        num_train_epochs = 30,
+        per_device_train_batch_size = 45,
+        per_device_eval_batch_size = 45,
+        warmup_steps = 500,
+        weight_decay = 0.01,
+        logging_dir = "logs",
+        logging_steps = 200,
+        eval_strategy = "steps",
+        eval_steps = 1000,
+        save_steps = 1000,
+        save_total_limit = 3,
+        dataloader_num_workers = 8,
+        dataloader_prefetch_factor = 2,
+        report_to="wandb",
+        run_name="phobert-cls"
+    )
+    
+    trainer = Trainer(
+        model = model,
+        args = training_args,
+        train_dataset = train_dataset,
+        eval_dataset = val_dataset,
+        compute_metrics = compute_metric,
+    )
+    
+    trainer.train()
+
+if __name__ == "__main__":
+    main()
