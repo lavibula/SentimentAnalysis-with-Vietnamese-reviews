a5f7f3b3a0c99ccbdcc0f7e10cf208e990a58860
Đặng Kiều Trinh <Trinh.DK214933@sis.hust.edu.vn>
2024-06-04 01:59:12 +0700

    modify the path in setup.sh

--- model/ktrnh/setup.sh
+++ model/ktrnh/setup.sh
@@ -1,3 +1,7 @@
 rm -rf /opt/conda/lib/python3.10/site-packages/aiohttp-3.9.1.dist-info
-pip install -r requirements.txt
+
+pip install -q -r requirements.txt || echo "requirements.txt not found in the current directory"
+pip install -q -r model/ktrnh/requirements.txt || echo "requirements.txt not found in model/ktrnh/"
+pip install -q -r /kaggle/working/SentimentAnalysis-with-Vietnamese-reviews/model/ktrnh/requirements.txt || echo "requirements.txt not found in /kaggle/working/SentimentAnalysis-with-Vietnamese-reviews/model/ktrnh/"
+
 echo "Setup complete. All necessary packages have been installed."
